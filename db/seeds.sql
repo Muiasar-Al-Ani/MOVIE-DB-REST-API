8ff342b0625ab3fa2802ad48a76d8b9b63a7c880
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-08-19 21:57:58 -0400

    Added the seeds.sql file which will seed the movie_db with the starter movies names and reviews

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO movies (movie_name)
+VALUES ("TITANS: SEASON 3"),
+        ("BLACK WIDOW"),
+        ("JUNGLE CRUISE"),
+        ("BECKETT"),
+        ("THE SUICIDE SQUAD");
+
+INSERT INTO reviews (review) 
+VALUES ("A Massive Improvement from a borderline unwatchable 2nd Season. One of the biggest turnarounds in a TV show in recent memory."),
+        ("It was absolutely amazing! Felt like I was watching the continuation of the Civil War and before Infinity War! I was really impressed how the story seemed to flow with the other Marvel Movies!"),
+        ("Fantastic film that made me go through the five stages of grief, yet still left me wanting more. The production as a whole was truly a masterpiece and I cant wait to purchase the film on DVD. Looking forward to a visit to DisneyWorld or DisneyLand to see the creation of the next ride based on this film!"),
+        ("The movie takes obvious cues from '70s thrillers like Three Days of the Condor and Hitchcock masterpieces like North by Northwest, yet director Ferdinando Cito Filomarino, fails to capture the visceral paranoia and propulsive narrative momentum of either."),
+        ("From the bonkers fight choreography to the macabre logic driving the madcap action, this stylized comic book world looked nothing like ours, and was all the more transporting and entertaining for it.");
+
