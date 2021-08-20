0840702ba37bd15c672968debbe0b3cded312830
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-08-19 21:33:12 -0400

    Added the schema.sql that will create the movie_db with it's 2 tables the movies and the reviews

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+
+CREATE DATABASE movie_db;
+
+USE movie_db;
+
+CREATE TABLE movies (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    movie_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE reviews (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    movie_id INT NOT NULL,
+    review TEXT NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE SET NULL
+);
